2793c7c40e48fbc91d65acbcd550764bb55f7a2d
klevi43 <75236133+klevi43@users.noreply.github.com>
2022-07-14 19:00:33 +0900

    Delete homepage.js

--- routes/homepage.js
+++
@@ -1,9 +0,0 @@
-const express  = require('express');
-
-const homepageController = require('../controllers/homepage');
-
-const router = express.Router();
-
-router.get('/', homepageController.getHomepage);
-
-module.exports = router;
