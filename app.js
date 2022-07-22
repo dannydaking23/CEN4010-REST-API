55f628cb082888feeba2ec97b4b2a610f5664273
dannydaking23 <77596497+dannydaking23@users.noreply.github.com>
2022-07-22 01:44:37 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const mysql = require('mysql')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(morgan('short'))
+
+const profileRouter = require('./routes/profile.js')
+const shoppingCartRouter = require('./routes/shoppingCart.js')
+
+app.use(profileRouter)
+app.use(shoppingCartRouter)
+
+// localhost:3003
+app.listen(3003, () => {
+    console.log("Server is up and listening on 3003...")
+})
