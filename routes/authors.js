13e7e80223dbc3626825efce5943be4f412e4fae
klevi43 <75236133+klevi43@users.noreply.github.com>
2022-07-23 11:15:33 +0900

    Add files via upload

--- routes/authors.js
+++ routes/authors.js
@@ -1,16 +1,113 @@
+// IMPORTS SECTION
+
 // imports express
 const express = require('express');
 
+// imports mysql
+const mysql = require('mysql');
+
+// allows use of express functions
 const router = express.Router();
 
-// imports Author Model
-const AuthorModel = require('../models/author');
+//********************************************************************** */
+
+// DATABASE CONNECTION SECTION
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: '', // Enter your database folder name 
+    password: '' // Enter your MySQL Workbench password
+});
+
+
+//********************************************************************** */
+
+// CLASS/OBJECT SECTION
+
+    // JSON fields
+const Author = {
+    // JSON data format
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    biography: {
+        type: String
+    },
+    publisher: {
+        type: String
+    }
+}
+// Class/Object methods
+// GET all authors in authors table in database
+Author.getAuthors = (req, res, next) => {
+    console.log('Authors and Books Called');
+    db.query('SELECT * FROM authors', (err, result) => {
+        if(err) {
+            res.sendStatus(500);
+            throw err;
+        }
+        else {
+        res.json(result);
+        }
+    });
+}
+
+// GET all books by a particular author in the database
+Author.getAuthorBooks = (req, res, next) => {
+    console.log('Authors and Books Called');
+    db.query('SELECT books.* FROM authors JOIN books ON books.author = CONCAT(authors.firstName, " ",  authors.lastName) WHERE books.author = ?', [req.params.authors], (err, result) => {
+        if(err) {
+            res.sendStatus(500);
+            throw err;
+        }
+        else {
+        res.json(result);
+        }
+    });
+}
+
+// ADD new author
+Author.addNewAuthor = (req, res, next) => {
+    // params for author database table
+    
+    let firstName = req.body.firstName
+    let lastName = req.body.lastName
+    let biography = req.body.biography
+    let publisher = req.body.publisher
+    
+    if(!firstName || !lastName || !biography || !publisher) {
+        return res.status(400).send({Error: true, message: "One or more fields are empty"})
+    }
+    const queryString = "INSERT INTO authors (firstName, lastName, biography, publisher) VALUES ( ?, ?, ?, ?)"
+    db.query(queryString, [firstName, lastName, biography, publisher], (err, results, fields) => {
+        if(err) {
+            if(err.errno == 1062){
+                return res.status(400).send({Error: true, message: "Author already exists"})
+            }
+            else {
+            res.sendStatus(400);
+            throw err;
+            }
+        }
+        else {
+            
+            res.end(JSON.stringify(results));
+        }
+    });
+}
 
 // route to GET all authors
-router.get('/authors', AuthorModel.getAuthors);
+router.get('/authors', Author.getAuthors);
 
 // route to GET all books by a certain author
-router.get('/:authors/books', AuthorModel.getAuthorBooks);
+router.get('/:authors/books', Author.getAuthorBooks);
+
+// route to POST new author to database
+router.post('/admin/add-author', Author.addNewAuthor);
 
 // exports router
 module.exports = router;
--- routes/books.js
+++ routes/books.js
@@ -1,19 +1,134 @@
+// IMPORTS SECTION
+
 // imports express
 const express = require('express');
 
+// imports mysql
+const mysql = require('mysql');
 
-// imports Book Model 
-const BookModel = require('../models/book');
-
+// allows use of express functions
 const router = express.Router();
 
-// route to GET all products in database
-router.get('/books', BookModel.getAllBooks);
+//********************************************************************** */
+
+// DATABASE CONNECTION SECTION
+
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: '', // Enter your database folder name 
+    password: '' // Enter your MySQL Workbench password
+});
 
 
+//********************************************************************** */
+
+// CLASS/OBJECT SECTION
+
+    // JSON fields
+let Book = {
+    // JSON data format
+    isbn: {
+        type: String
+    },
+    title: {
+        type: String
+    },
+    author: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    price: {
+        type: Number
+    },
+    genre: {
+        type: String
+    },
+    publisher: {
+        type: String
+    },
+    yearPublished: {
+        type: String
+    },
+    copiesSold: {
+        type: Number
+    }
+}
+
+
+// Class/Object methods
+
+// GET all books in database
+Book.getAllBooks = (req, res, next) => {
+    console.log('Showing all books in database');
+    db.query('SELECT * FROM books', (err, result) => {
+        if(err) {
+            res.sendStatus(500);
+            throw err;
+        }
+        else{
+            console.log("Here are the requested books: ")
+            res.json(result);
+        }
+    });
+}
+
+// GET all books by ISBN
+Book.getBookByISBN = (req, res, next) => {
+    console.log('Books by ISBN Called');
+    db.query('SELECT * FROM books WHERE isbn = ?', [req.params.isbn], (err, result) => {
+        if(err) {
+            res.sendStatus(500);
+            throw err;
+        }
+        else{
+            console.log("Here is the requested book: ");
+            res.json(result);
+        }
+    });
+}
+// ADD new book to database
+Book.addNewBook = (req, res, next) => {
+    // params for book database table
+    let isbn = req.body.isbn
+    let title = req.body.title
+    let author = req.body.author
+    let description = req.body.description
+    let price = req.body.price
+    let genre = req.body.genre
+    let publisher = req.body.publisher
+    let yearPublished = req.body.yearPublished
+    let copiesSold = req.body.copiesSold
+    
+    if(!isbn || !title || !author || !description || !price || !genre || !publisher || !yearPublished || !copiesSold) {
+        return res.status(400).send({Error: true, message: "One or more fields are empty"})
+    }
+    console.log(isbn)
+    const queryString = "INSERT INTO books (isbn, title, author, description, price, genre, publisher, yearPublished, copiesSold) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ? )"
+    db.query(queryString, [isbn, title, author, description, price, genre, publisher, yearPublished, copiesSold], (err, results, fields) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            console.log("Book Successfully inserted into database");
+            res.end(JSON.stringify(results));
+        }
+    })
+}
+
+//********************************************************************** */
+
+// ROUTER METHOD SECTION
+
+// route to GET all books in database
+router.get('/books', Book.getAllBooks);
 
 // route to GET books by isbn
-router.get('/books/:isbn', BookModel.getBookByISBN);
+router.get('/books/:isbn', Book.getBookByISBN);
 
+// route to POST new book to database
+router.post('/admin/add-book', Book.addNewBook)
 // exports router
 module.exports = router;
