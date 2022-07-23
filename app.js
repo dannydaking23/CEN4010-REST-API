121ac7136a13a13e6825cc81c50ae10aa1707a63
klevi43 <75236133+klevi43@users.noreply.github.com>
2022-07-23 11:21:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -11,10 +11,14 @@ app.use(morgan('short'))
 const profileRouter = require('./routes/profile.js')
 const shoppingCartRouter = require('./routes/shoppingCart.js')
 const wishlistRouter = require('./routes/wishlist.js')
+const bookRouter = require('./routes/books.js')
+const authorRouter = require('./routes/authors.js')
 
 app.use(profileRouter)
 app.use(shoppingCartRouter)
 app.use(wishlistRouter)
+app.use(bookRouter)
+app.use(authorRouter)
 
 // localhost:3003
 app.listen(3003, () => {
